7f4bf048186544fea51b1a79a3ec7f43783d7c86
Eli Itah <eitah@cars.com>
2018-12-22 10:12:46 -0600

    Store model finished. Store Controller finished although post just writes to the window. Hello module is not needed. Default content is probably not neeeded. Store partial mixin finished

---
+++ models/Store.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+const slug = require("slugs");
+
+// do all your data normalization close to the model as possible
+const storeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: "Please enter a store name!"
+  },
+  slug: String,
+  description: {
+    type: String,
+    trim: true
+  },
+  tags: [String]
+});
+
+storeSchema.pre('save', function(next) {
+    if (!this.isModified('name')) {
+        next() // ship it
+        return; // stop this function from running
+    }
+    this.slug = slug(this.name); // turns the store name into a slug
+    next();
+    // TODO make more resillient so slugs are unique
+})
+
+module.exports = mongoose.model("Store", storeSchema);

1255db69a25af02a2a03ae7c848f7d544cf209cf
Eli Itah <eitah@cars.com>
2018-12-22 18:06:52 -0600

    Store add and edit completed. Start of lat long available.

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,12 +1,51 @@
+const mongoose = require("mongoose");
+const Store = mongoose.model("Store");
+
 exports.homePage = (req, res) => {
-    console.log(req.name)
-    res.render('index');
+  res.render("index");
 };
 
 exports.addStore = (req, res) => {
-    res.render("editStore", { title: "💩 Add Store" });
+  res.render("editStore", { title: "💩 Add Store" });
 };
 
-exports.createStore = (req, res) => {
-    res.json(req.body);
+// types of flash: success warning info error
+exports.createStore = async (req, res) => {
+  const store = new Store(req.body);
+  await store.save();
+  console.log("it worked!");
+  req.flash(
+    "success",
+    `Successfully created ${store.name}. Care to leave a review?`
+  );
+  res.redirect(`/store/${store.slug}`);
+};
+ 
+exports.getStores = async (req, res) => {
+  // query the db for a list of all stores
+  const stores = await Store.find();
+  res.render("stores", { title: "stores", stores });
+};
+
+exports.editStore = async (req, res) => {
+  // find the store given the id
+  const store = await Store.findOne({ _id: req.params.id });
+  // TODO confirm they are the owner of the store
+  // renders out the edit form so the user can update their store
+  res.render('editStore', { title: `Edit ${store.name}`, store});
+};
+
+exports.updateStore = async (req, res) => {
+  //find and update store with args: query, data, optio ns. 
+  // Exec() ensures it runs.
+
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true, // return the new store instead of the old ones,
+    runValidators: true,
+  }).exec();
+  // tell them the store was updated
+  req.flash('success', `Successfully updated <strong>${store.name}</strong>.
+  <a href="/stores/${store.slug}">View Store →</a>`)
+  res.redirect(`/stores/${store._id}/edit`);
+  // redirect to the store and tell them it worked
 };
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ const express = require("express");
 const router = express.Router();
 const storeController = require("../controllers/storeController");
 
-router.get("/", storeController.homePage);
-router.get("/add", storeController.addStore);
-router.post("/add", storeController.createStore);
+const { catchErrors } = require('../handlers/errorHandlers');
+
+router.get("/", catchErrors(storeController.getStores));
+router.get("/stores", catchErrors(storeController.getStores));
+router.get("/add", (storeController.addStore));
+router.post("/add/:id", catchErrors(storeController.updateStore));
+router.post("/add", catchErrors(storeController.createStore));
+router.get("/stores/:id/edit", catchErrors(storeController.editStore));
 
 module.exports = router;
